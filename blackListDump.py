9a99b807eb1d477e6a0e50108bcd5ea189f7c28d
Bsebring <46118405+Bsebring@users.noreply.github.com>
2019-06-20 13:12:30 -0400

    Add files via upload

--- blackListDump.py
+++ blackListDump.py
@@ -11,12 +11,21 @@ headers = {
 }
 
 response = requests.request(method='GET', url=url, headers=headers)
+decodedResponse = json.loads(response.text)
 
 if response.status_code == 200:
     with open('blacklist.txt', 'w') as outFile:
         outFile.write(response.text)
-    with open('blacklist.txt', 'r') as inFile:
-        with open('/etc/csf/csf.deny', 'a+') as outFile:
+    with open('blacklist.txt', 'r') as inFile, \
+         open('/etc/csf/csf.deny', 'a+') as outFile:
             outFile.write(inFile.read())
+elif response.status_code == 429:
+    print (json.dumps(decodedResponse['errors'][0], sort_keys=True, indent=4))
+elif response.status_code == 422:
+    print (json.dumps(decodedResponse['errors'], sort_keys=True, indent=4))
+elif response.status_code == 302:
+    print('Unsecure protocol requested. Redirected to HTTPS.')
+elif response.status_code == 401:
+    print (json.dumps(decodedResponse['errors'][0]['detail'], sort_keys=True, indent=4))
 else:
-    print('Error contacting the server')
+    print('Unexpected server response. Status Code:' response.status_code)
